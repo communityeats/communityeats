76a214dcca5076a5f63e4fea3745f45b593ecd56
Michael Han <mykalehan@gmail.com>
2026-01-07 20:36:42 +1100

    unbreak build

--- src/app/l/[id]/[[...slug]]/page.tsx
+++ src/app/l/[id]/[[...slug]]/page.tsx
@@ -2,7 +2,12 @@ import { redirect } from 'next/navigation'
 
 type Params = { id: string; slug?: string[] }
 
-export default function ShortListingRedirect({ params }: { params: Params }) {
-  const target = `/listings/${params.id}`
+export default async function ShortListingRedirect({
+  params,
+}: {
+  params: Promise<Params>
+}) {
+  const resolvedParams = await params
+  const target = `/listings/${resolvedParams.id}`
   redirect(target)
 }
